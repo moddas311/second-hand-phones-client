0e1b5112e589a41caf190c75946a4038587af762
moddas311 <moddasir311@gmail.com>
2022-11-25 02:21:29 +0600

    home page updeted

---
+++ src/components/pages/Home/Home/HomeReview/HomeReview.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import background from '../../../../../assets/banner/background.jpg';
+
+const HomeReview = () => {
+    return (
+        <div className='text-center my-10 px-5
+            bg-gradient-to-r from-black to-transparent'
+            style={{
+                background: `url(${background})`
+            }}
+        >
+            <div className='py-5'>
+                <h4 className='text-xl font-bold text-primary'>Connect Us</h4>
+                <h3 className='text-2xl text-yellow-500 font-bold'>For Any Information</h3>
+            </div>
+            <form>
+                <div>
+                    <input type="email" placeholder="Email Address" className="input input-bordered input-secondary w-full max-w-xs" />
+                </div>
+                <div className='my-4'>
+                    <input type="text" placeholder="Subject" className="input input-bordered input-secondary w-full max-w-xs" />
+                </div>
+                <div>
+                    <textarea className="textarea w-full input-bordered input-secondary max-w-xs" placeholder="Your Opinion"></textarea>
+                </div>
+                <div className='py-2'>
+                    <button className='btn btn-active btn-secondary rounded-lg'>Feed Back</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default HomeReview;
